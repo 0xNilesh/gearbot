e4a9e37c0ba989e9580baeca8c1aa940d02f4a5f
Nilesh Gupta <guptanilesh2312@gmail.com>
2023-11-17 23:37:21 +0530

    Added test for withdrawCollateral function

--- test/AddCollateralBot.t.sol
+++ test/AddCollateralBot.t.sol
@@ -30,10 +30,12 @@ contract AddCollateralBotTest is Test {
 
     address private constant USER = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
     address private constant EXECUTOR = 0x087F5052fBcD7C02DD45fb9907C57F1EccC2bE25;
-    address private constant VITALIK = 0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045;
+    address public creditAccount = address(0);
+    uint256 public ETH_AMOUNT = 100000000000000000000;
     uint192 ADD_COLLATERAL_PERMISSION = 1;
     uint192 REVOKE_ALLOWANCES_PERMISSION = 1 << 7;
     uint192 EXTERNAL_CALLS_PERMISSION = 1 << 16;
+    uint192 WITHDRAW_COLLATERAL_PERMISSION = 1 << 5;
 
     /// ----- ///
     /// SETUP ///
@@ -44,54 +46,54 @@ contract AddCollateralBotTest is Test {
     }
 
     function test_addCollateral() public {
+        console.log("///      ADD COLLATERAL LOGS        ///");
         MultiCall[] memory calls;
         vm.startPrank(USER);
 
-        console.log(IERC20(DAI).balanceOf(VITALIK));
-        console.log(IERC20(USDT).balanceOf(VITALIK));
-        console.log(IERC20(USDT).balanceOf(USER));
-
-        // console.log()
+        // creating credit account for USER (it'll wrap eth and send WETH to USER)
         address creditAccountCreated =
-            CreditFacadeV3(CREDIT_FACADE).openCreditAccount{value: 1000000000000000000000}(USER, calls, 0);
-        console.log(creditAccountCreated);
+            CreditFacadeV3(CREDIT_FACADE).openCreditAccount{value: ETH_AMOUNT}(USER, calls, 0);
+
+        creditAccount = creditAccountCreated;
 
         uint256 balanceOfUserBefore = IERC20(WETH).balanceOf(USER);
         console.log(balanceOfUserBefore);
 
-        // MultiCall[] memory callsForAddingCollateral = new MultiCall[](1);
-
-        // callsForAddingCollateral[0] = MultiCall({
-        //     target: CREDIT_FACADE,
-        //     callData: abi.encodeCall(ICreditFacadeV3Multicall.addCollateral, (WETH, balanceOfUserBefore))
-        // });
-
-        // IERC20(WETH).approve(CREDIT_MANAGER, balanceOfUserBefore);
+        // approve WETH for bot to add collateral
         IERC20(WETH).approve(address(bot), balanceOfUserBefore);
 
-        // CreditFacadeV3(CREDIT_FACADE).multicall(creditAccountCreated, callsForAddingCollateral);
+        uint192 permissionsBitmask = ADD_COLLATERAL_PERMISSION | REVOKE_ALLOWANCES_PERMISSION
+            | EXTERNAL_CALLS_PERMISSION | WITHDRAW_COLLATERAL_PERMISSION;
 
-        uint192 permissionsBitmask =
-            ADD_COLLATERAL_PERMISSION | REVOKE_ALLOWANCES_PERMISSION | EXTERNAL_CALLS_PERMISSION;
-
+        // settings permissions to bot to function onBehalfOf credit account of USER
         CreditFacadeV3(CREDIT_FACADE).setBotPermissions(creditAccountCreated, address(bot), permissionsBitmask);
         vm.stopPrank();
 
-        address botList = CreditFacadeV3(CREDIT_FACADE).botList();
-        console.log(address(bot));
-        (uint192 permissions, bool forbidden, bool hasSpecialPermissions) =
-            IBotListV3(botList).getBotStatus(address(bot), CREDIT_MANAGER, creditAccountCreated);
-        console.log(permissionsBitmask);
-        console.log(permissions);
-        console.log(forbidden);
-        console.log(hasSpecialPermissions);
-
         vm.startPrank(EXECUTOR);
 
+        // executing bot for adding collateral of USER's credit account (bot executed by EXECUTOR)
         bot.addCollateral(WETH, CREDIT_MANAGER, balanceOfUserBefore, creditAccountCreated, USER);
 
         uint256 balanceOfUserAfter = IERC20(WETH).balanceOf(USER);
         console.log(balanceOfUserAfter);
+        vm.stopPrank();
+    }
+
+    function test_withdrawCollateral() public {
+        test_addCollateral();
+        console.log("///      WITHDRAW COLLATERAL LOGS        ///");
+
+        vm.startPrank(EXECUTOR);
+
+        uint256 balanceOfUserBefore = IERC20(WETH).balanceOf(USER);
+        console.log(balanceOfUserBefore);
+
+        // executing bot for withdrawing collateral of USER's credit account (bot executed by EXECUTOR)
+        bot.withdrawCollateral(WETH, CREDIT_MANAGER, ETH_AMOUNT, creditAccount, USER);
+
+        uint256 balanceOfUserAfter = IERC20(WETH).balanceOf(USER);
+        console.log(balanceOfUserAfter);
+
         vm.stopPrank();
     }
 }
